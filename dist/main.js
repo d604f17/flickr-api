eecdc31d8699af5051dfab360de071e3a62e852e
rasmus@rhnorskov.com <rasmus@rhnorskov.com>
2017-03-14 14:53:56 +0100

    made a package

--- dist/main.js
+++ dist/main.js
@@ -1,40 +1,47 @@
 'use strict';
 
-var Flickr = require('flickrapi');
-var express = require('express');
-
-var app = express();
-
-app.get('/', function (req, res) {
-    res.json({
-        name: 'flickr api',
-        version: '0.0.1',
-        authors: ['Rasmus Nørskov (rhnorskov)', 'Mathias Wieland (cuntbag)', 'Andreas Sommerset (flapjack)']
-    });
+Object.defineProperty(exports, "__esModule", {
+    value: true
 });
 
-app.get('/:text', function (req, res) {
-    req.params.text;
-    Flickr.tokenOnly({
-        api_key: "85f11febb88e3a4d49342a95b7bcf1e8",
-        secret: "429ecaa952bd0757"
-    }, function (error, flickr) {
-        flickr.photos.search({
-            text: req.params.text
-        }, function (error, result) {
-            if (!error) {
-                result.photos.photo = result.photos.photo.map(function (photo) {
-                    return Object.assign(photo, { url: 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg' });
-                });
-                res.json(result);
-            }
-        });
-    });
-});
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-// export FLICKR_USER_ID="84174915@N08"
-// export FLICKR_ACCESS_TOKEN="72157677679254294-afea93d392d1ac88"
-// export FLICKR_ACCESS_TOKEN_SECRET="ea8c60ebe0656431"
+var _qs = require('qs');
 
-app.listen(process.env.port || 4000);
+var _qs2 = _interopRequireDefault(_qs);
+
+var _requestPromise = require('request-promise');
+
+var _requestPromise2 = _interopRequireDefault(_requestPromise);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Flickr = function () {
+    function Flickr(options) {
+        _classCallCheck(this, Flickr);
+
+        this.api_key = options.api_key;
+        this.format = options.format;
+        this.url = 'https://api.flickr.com/services/rest' || options.url;
+    }
+
+    _createClass(Flickr, [{
+        key: 'query',
+        value: function query(method, parameters) {
+            var query = _qs2.default.stringify(Object.assign({
+                api_key: this.api_key,
+                method: 'flickr.' + method,
+                format: this.format
+            }, parameters));
+
+            return (0, _requestPromise2.default)(this.url + '?' + query);
+        }
+    }]);
+
+    return Flickr;
+}();
+
+exports.default = Flickr;
 //# sourceMappingURL=main.js.map
--- main.js
+++ main.js
@@ -1,41 +1,20 @@
-const Flickr = require('flickrapi');
-const express = require('express');
+import qs from 'qs';
+import rp from 'request-promise';
 
-const app = express();
+export default class Flickr {
+    constructor(options) {
+        this.api_key = options.api_key;
+        this.format = options.format;
+        this.url = 'https://api.flickr.com/services/rest' || options.url;
+    }
 
-app.get('/', (req, res) => {
-    res.json({
-        name: 'flickr api',
-        version: '0.0.1',
-        authors: [
-            'Rasmus Nørskov (rhnorskov)',
-            'Mathias Wieland (cuntbag)',
-            'Andreas Sommerset (flapjack)'
-        ]
-    })
-});
+    query(method, parameters) {
+        let query = qs.stringify(Object.assign({
+            api_key: this.api_key,
+            method: 'flickr.' + method,
+            format: this.format
+        }, parameters));
 
-app.get('/:text', (req, res) => {
-    req.params.text
-    Flickr.tokenOnly({
-        api_key: "85f11febb88e3a4d49342a95b7bcf1e8",
-        secret: "429ecaa952bd0757"
-    }, function (error, flickr) {
-        flickr.photos.search({
-            text: req.params.text
-        }, function (error, result) {
-            if (!error) {
-                result.photos.photo = result.photos.photo.map(photo => {
-                    return Object.assign(photo, {url: `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`})
-                });
-                res.json(result)
-            }
-        });
-    });
-});
-
-// export FLICKR_USER_ID="84174915@N08"
-// export FLICKR_ACCESS_TOKEN="72157677679254294-afea93d392d1ac88"
-// export FLICKR_ACCESS_TOKEN_SECRET="ea8c60ebe0656431"
-
-app.listen(process.env.port || 4000);
+        return rp(this.url + '?' + query);
+    }
+}
